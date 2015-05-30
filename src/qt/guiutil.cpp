9622e028fb9c174de7df1593a9e232abe23f886b
nzsquirrell <nzsquirrell@users.noreply.github.com>
2015-05-30 19:24:37 +1200

    New logo + minor GUI changes

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -97,7 +97,7 @@ void setupAddressWidget(QValidatedLineEdit *widget, QWidget *parent)
 
     widget->setFont(bitcoinAddressFont());
 #if QT_VERSION >= 0x040700
-    widget->setPlaceholderText(QObject::tr("Enter a Unitus address (e.g. 1NS17iag9jJgTHD1VXjvLCEnZuQ3rJDE9L)"));
+    widget->setPlaceholderText(QObject::tr("Enter a Unitus address (e.g. UeMjHZkbFSkJL9PmapMn1HrhmkNvXu2gP7)"));
 #endif
     widget->setValidator(new BitcoinAddressEntryValidator(parent));
     widget->setCheckValidator(new BitcoinAddressCheckValidator(parent));
