b176229216faff40b7c1f88c00ffa3b8af0a7c3b
nzsquirrell <squirrell@nutty.one>
2017-08-09 16:23:32 +1200

    fix for merge mining. we're not deprecating target param in getauxblock

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -951,7 +951,7 @@ UniValue getauxblock(const JSONRPCRequest& request)
             "  \"coinbasevalue\"      (numeric) value of the block's coinbase\n"
             "  \"bits\"               (string) compressed target of the block\n"
             "  \"height\"             (numeric) height of the block\n"
-            "  \"_target\"            (string) target in reversed byte order, deprecated\n"
+            "  \"target\"             (string) target in reversed byte order\n"
             "}\n"
             "\nResult (with arguments):\n"
             "xxxxx        (boolean) whether the submitted block was correct\n"
@@ -1056,7 +1056,7 @@ UniValue getauxblock(const JSONRPCRequest& request)
         result.push_back(Pair("coinbasevalue", (int64_t)pblock->vtx[0]->vout[0].nValue));
         result.push_back(Pair("bits", strprintf("%08x", pblock->nBits)));
         result.push_back(Pair("height", static_cast<int64_t> (pindexPrev->nHeight + 1)));
-        result.push_back(Pair("_target", HexStr(BEGIN(target), END(target))));
+        result.push_back(Pair("target", HexStr(BEGIN(target), END(target))));
 
         return result;
     }
