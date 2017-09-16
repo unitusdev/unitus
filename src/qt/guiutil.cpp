3eccc39c1ff725be68d82b749ddb34ab95c277f5
HashToBeWild <hashtobewild@cryptocurrency.ninja>
2017-09-16 16:38:24 -0400

    Fixed address entry font

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -129,7 +129,7 @@ void setupAddressWidget(QValidatedLineEdit *widget, QWidget *parent)
 {
     parent->setFocusProxy(widget);
 
-    widget->setFont(fixedPitchFont());
+    //widget->setFont(fixedPitchFont());
 #if QT_VERSION >= 0x040700
     // We don't want translators to use own addresses in translations
     // and this is the only place, where this address is supplied.
