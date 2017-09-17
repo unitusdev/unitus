4f6a2e2df481eb9442d276f60d91c299174c4a87
HashToBeWild <hashtobewild@cryptocurrency.ninja>
2017-09-17 03:46:21 -0400

    Darker Splash Text

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -59,7 +59,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle *networkStyle) 
 #endif
 
     QPainter pixPaint(&pixmap);
-    pixPaint.setPen(QColor(255,255,255));
+    pixPaint.setPen(QColor(62, 62, 62));
 
     // draw a slightly radial gradient
     //QRadialGradient gradient(QPoint(0,0), splashSize.width()/devicePixelRatio);
