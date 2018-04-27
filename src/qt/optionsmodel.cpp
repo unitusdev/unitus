b3527b3d91de75c2e2373da0f927998edb4447df
nzsquirrell <squirrell@nutty.one>
2018-04-28 09:22:25 +1200

    change explorer url

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -74,7 +74,7 @@ void OptionsModel::Init(bool resetSettings)
     nDisplayUnit = settings.value("nDisplayUnit").toInt();
 
     if (!settings.contains("strThirdPartyTxUrls"))
-        settings.setValue("strThirdPartyTxUrls", "https://explorer.unitus.online/tx/%s");
+        settings.setValue("strThirdPartyTxUrls", "https://explorer.unitus.network/tx/%s");
     strThirdPartyTxUrls = settings.value("strThirdPartyTxUrls", "").toString();
 
     if (!settings.contains("fCoinControlFeatures"))
